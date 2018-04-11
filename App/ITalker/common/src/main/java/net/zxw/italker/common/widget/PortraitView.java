1cd71aa63291259c2fe7eaca9312ad71805b7994
zengxingwei <443075929@qq.com>
2018-04-11 16:16:33 +0800

    项目4..1提交，圆形头像

---
+++ App/ITalker/common/src/main/java/net/zxw/italker/common/widget/PortraitView.java
@@ -0,0 +1,26 @@
+package net.zxw.italker.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * @author zxw
+ * @Email 18316275391@163.com
+ */
+
+public class PortraitView extends CircleImageView{
+
+    public PortraitView(Context context) {
+        super(context);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
